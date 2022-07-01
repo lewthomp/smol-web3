afb53c76e0c8cdf01d88929d97eec394cfc81ccb
rvdemonk <rvdemonk@gmail.com>
2022-07-01 18:54:54 +1000

    mock nft contract finished

--- src/SmolOpenSea.sol
+++ src/SmolOpenSea.sol
@@ -2,15 +2,21 @@
 
 pragma solidity ^0.8.15;
 
-import { ERC721 } from "solmate/tokens/ERC721.sol";
+import {ERC721} from "solmate/tokens/ERC721.sol";
 
-contract MockNFT is ERC721('MockNFT', 'MOCK') {
+contract MockNFT is ERC721 {
     uint256 public supply = 0;
 
-    // constructor() ERC721('MockNFT', 'MOCK') {}
+    constructor() ERC721("MockNFT", "MOCK") {}
 
-    function tokenURI() public pure override returns (string memory) {
-        // string memory uri = string(_tokenId);
-        return 'test';
+    function tokenURI(uint256) public pure override returns (string memory) {
+        return "test uri";
+    }
+
+    function mint(address to) public payable {
+        _mint(to, supply);
+        supply += 1;
     }
 }
+
+contract SmolOpenSea {}
